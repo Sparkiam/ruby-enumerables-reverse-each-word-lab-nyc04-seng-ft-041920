2024654b100e5a6ad3a8e6b7eabd56927e0b5446
Dell Wilson <warren.wilsonjr89@gmail.com>
2020-05-09 23:12:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,12 @@
 def reverse_each_word(string)
+  # reversed = []
+  # sentence.split.map do |word|
+  #   reversed << word.reverse
+  # end
+  # return reversed.join(' ')
+  
+  
+  
   new_string = string.split("")
   array = []
   string.size.times { array << split_string.pop}
